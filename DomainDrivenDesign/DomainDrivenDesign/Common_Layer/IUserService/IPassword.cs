52a38312b9f4746ca75bd68469514bb32f17e101
Burak Seçer <brkscr@hotmail.com.tr>
2019-11-27 14:26:49 +0300

    CommonLayer add Interfaces

---
+++ DomainDrivenDesign/DomainDrivenDesign/Common_Layer/IUserService/IPassword.cs
@@ -0,0 +1,8 @@
+﻿namespace Common_Layer.IUserService
+{
+    public interface IPassword
+    {
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
+    }
+}
---
+++ DomainDrivenDesign/DomainDrivenDesign/Common_Layer/IUserService/IToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common_Layer.IUserService
+{
+    public interface IToken
+    {
+        string CreateToken(string secretKey, string issuer, string audience, Claim[] claims, DateTime expireTime);
+        string GetValue(string token, string claimName);
+        int GetUserId(string token);
+        string GetPermissionList(string token);
+    }
+}
